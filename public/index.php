9a38ddc1f68a665246fe23aa5d00a9afd0842613
SUNSHINE POSERIO <sunshineposerio@gmail.com>
2025-07-15 20:55:59 +0800

    Fix bootstrap logic in public/index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+define('LARAVEL_START', microtime(true));
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
